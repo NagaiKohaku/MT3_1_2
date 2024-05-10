5beb120bd061ee16f363c06a1aa6aad327dd6ce0
NagaiKohaku <k023g0037@g.neec.ac.jp>
2024-05-10 11:01:36 +0900

    逆行列の式のミスを修正

--- Pipeline.cpp
+++ Pipeline.cpp
@@ -242,7 +242,7 @@ Matrix4x4 Pipeline::Inverse(const Matrix4x4& m) {
 			m.m[1][2] * m.m[2][3] * m.m[3][0] -
 			m.m[1][3] * m.m[2][0] * m.m[3][2] +
 			m.m[1][3] * m.m[2][2] * m.m[3][0] +
-			m.m[1][2] * m.m[2][1] * m.m[3][3] +
+			m.m[1][2] * m.m[2][0] * m.m[3][3] +
 			m.m[1][0] * m.m[2][3] * m.m[3][2]
 			);
 
@@ -382,7 +382,7 @@ Matrix4x4 Pipeline::MakePerspectiveFovMatrix(float fovY, float aspectRatio, floa
 
 	Matrix4x4 result;
 
-	result.m[0][0] = 1.0f / aspectRatio * cot(fovY / 2.0f);
+	result.m[0][0] = (1.0f / aspectRatio) * cot(fovY / 2.0f);
 	result.m[0][1] = 0.0f;
 	result.m[0][2] = 0.0f;
 	result.m[0][3] = 0.0f;
